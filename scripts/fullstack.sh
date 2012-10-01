8ca9bda549ab26f79c084446c84991c4212bde71
Jonathan Senior <jonathan@opengamma.com>
2012-10-01 11:50:01 +0100

    Updating to latest deployment

--- scripts/fullstack.sh
+++ scripts/fullstack.sh
@@ -1,12 +1,31 @@
 #!/bin/bash
-BASENAME=${0##*/}                                                                                                               
-COMPONENT=${BASENAME%.sh}                                                                                                       
-BASEDIR="$(dirname $(dirname $(readlink -f $0)))"                                                                               
-SCRIPTDIR=${BASEDIR}/scripts
-PROJECT=exampleserver
 
+canonicalize() {
+  local _TARGET _BASEDIR
+  _TARGET="$0"
+  readlink -f $_TARGET 2>/dev/null || (
+    cd $(dirname "$_TARGET")
+    _TARGET=$(basename "$_TARGET")
+
+    while [ -L "$_TARGET" ]
+    do
+      _TARGET=$(readlink "$_TARGET")
+      cd $(dirname "$_TARGET")
+      _TARGET=$(basename "$_TARGET")
+    done
+    _BASEDIR=$(pwd -P)
+    echo "$_BASEDIR/$_TARGET"
+  )
+}
+
+BASENAME=${0##*/}
+COMPONENT=${BASENAME%.sh}
+BASEDIR="$(dirname "$(dirname "$(canonicalize "$0")")")"
+SCRIPTDIR=${BASEDIR}/scripts
 cd "${BASEDIR}" || exit 1
 
+. ${SCRIPTDIR}/project-utils.sh
+. ${SCRIPTDIR}/java-utils.sh
 . ${SCRIPTDIR}/componentserver-init-utils.sh
 
 # Read default configs
@@ -15,6 +34,7 @@ load_default_config
 # Component specific default configs
 CONFIG=classpath:fullstack/fullstack-dev.properties
 LOGBACK_CONFIG=fullstack/fullstack-logback.xml
+LOGFILE=logs/fullstack-console.log
 
 # User customizations
 load_component_config ${PROJECT} ${COMPONENT}
@@ -33,12 +53,18 @@ case "$1" in
   status)
     status
     ;;
+  debug)
+    debug
+    ;;
+  showconfig)
+    showconfig
+    ;;
   restart|reload)
     stop
     start
     ;;
   *)
-    echo "Usage: $0 {start|stop|restart|status|reload}"
+    echo "Usage: $0 {start|stop|restart|status|showconfig|debug|reload}"
 esac
 
 exit ${RETVAL}
